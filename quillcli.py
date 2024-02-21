b41ee59f8cb7ebb94fed2a6b026a555309d1c8b5
mmwandera <mmwandera74@gmail.com>
2024-02-21 17:32:44 +0300

    added styling

--- quillcli.py
+++ quillcli.py
@@ -4,7 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from models import Base, User, BlogPost, Comment
 from prettytable import PrettyTable
 from passlib.hash import bcrypt
-import getpass  # Import getpass for secure password input
+import getpass
+from colorama import init, Fore, Style
+
+# Initialize colorama for text styling
+init(autoreset=True)
+
+# Styling constants
+HEADER_COLOR = Fore.CYAN + Style.BRIGHT
+SUCCESS_COLOR = Fore.GREEN
+ERROR_COLOR = Fore.RED
+PROMPT_COLOR = Fore.YELLOW
+RESET_STYLE = Style.RESET_ALL
 
 # Create an SQLAlchemy engine to interact with the database
 engine = create_engine('sqlite:///quillcli.db')
@@ -17,33 +28,33 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def get_secure_input(prompt):
-    return getpass.getpass(prompt)
+    return getpass.getpass(PROMPT_COLOR + prompt + RESET_STYLE)
 
 def sign_up():
-    print("Sign Up")
-    username = input("Enter username: ")
+    print(HEADER_COLOR + "Sign Up")
+    username = input(PROMPT_COLOR + "Enter username: ")
     email = input("Enter email: ")
     password = get_secure_input("Enter password: ")
     hashed_password = bcrypt.hash(password)
 
     new_user = User.create_user(session, username, email, hashed_password)
-    print("User signed up successfully.")
+    print(SUCCESS_COLOR + "User signed up successfully.")
 
 def sign_in():
-    print("Sign In")
-    username = input("Enter username: ")
+    print(HEADER_COLOR + "Sign In")
+    username = input(PROMPT_COLOR + "Enter username: ")
     password = get_secure_input("Enter password: ")
 
     user = User.authenticate(session, username, password)
     if user:
-        print(f"Welcome, {user.username}!")
+        print(SUCCESS_COLOR + f"Welcome, {user.username}!")
         return user
     else:
-        print("Invalid username or password.")
+        print(ERROR_COLOR + "Invalid username or password.")
         return None
 
 def view_all_blog_posts():
-    print("View All Blog Posts")
+    print(HEADER_COLOR + "View All Blog Posts")
     all_blog_posts = session.query(BlogPost).all()
     
     table = PrettyTable()
@@ -52,11 +63,11 @@ def view_all_blog_posts():
         table.add_row([post.id, post.title, post.user.username, post.created_at])
     print(table)
 
-    blog_post_id = input("Enter blog post ID to view details (or 0 to go back): ")
+    blog_post_id = input(PROMPT_COLOR + "Enter blog post ID to view details (or 0 to go back): ")
     try:
         blog_post_id = int(blog_post_id)
     except ValueError:
-        print("Invalid blog post ID.")
+        print(ERROR_COLOR + "Invalid blog post ID.")
         return
 
     if blog_post_id == 0:
@@ -67,7 +78,7 @@ def view_all_blog_posts():
         view_blog_post_details(blog_post)
 
 def view_blog_post_details(blog_post):
-    print("View Blog Post Details")
+    print(HEADER_COLOR + "View Blog Post Details")
     print(f"Title: {blog_post.title}")
     print(f"Content: {blog_post.content}")
     print(f"Author: {blog_post.user.username}")
@@ -79,7 +90,7 @@ def view_blog_post_details(blog_post):
         print("2. Add Comment")
         print("0. Back")
 
-        choice = input("Enter your choice: ")
+        choice = input(PROMPT_COLOR + "Enter your choice: ")
 
         if choice == "1":
             view_comments(blog_post)
@@ -88,10 +99,10 @@ def view_blog_post_details(blog_post):
         elif choice == "0":
             break
         else:
-            print("Invalid choice. Please enter a number from the menu.")
+            print(ERROR_COLOR + "Invalid choice. Please enter a number from the menu.")
 
 def view_comments(blog_post):
-    print("View Comments")
+    print(HEADER_COLOR + "View Comments")
     comments = Comment.get_comments_for_blog_post(session, blog_post.id)
     
     table = PrettyTable()
@@ -101,22 +112,22 @@ def view_comments(blog_post):
     print(table)
 
 def add_comment(user, blog_post):
-    print("Add Comment")
-    text = input("Enter your comment: ")
+    print(HEADER_COLOR + "Add Comment")
+    text = input(PROMPT_COLOR + "Enter your comment: ")
     
     new_comment = Comment.create_comment(session, text, user.id, blog_post.id)
-    print("Comment added successfully.")
+    print(SUCCESS_COLOR + "Comment added successfully.")
 
 def add_blog_post(user):
-    print("Add Blog Post")
-    title = input("Enter blog post title: ")
+    print(HEADER_COLOR + "Add Blog Post")
+    title = input(PROMPT_COLOR + "Enter blog post title: ")
     content = input("Enter blog post content: ")
 
     new_blog_post = BlogPost.create_blog_post(session, title, content, user.id)
-    print("Blog post added successfully.")
+    print(SUCCESS_COLOR + "Blog post added successfully.")
 
 def delete_blog_post(user):
-    print("Delete Blog Post")
+    print(HEADER_COLOR + "Delete Blog Post")
     user_blog_posts = BlogPost.get_blog_posts_by_user(session, user.id)
 
     if not user_blog_posts:
@@ -129,11 +140,11 @@ def delete_blog_post(user):
         table.add_row([post.id, post.title, post.created_at])
     print(table)
 
-    blog_post_id = input("Enter blog post ID to delete (or 0 to go back): ")
+    blog_post_id = input(PROMPT_COLOR + "Enter blog post ID to delete (or 0 to go back): ")
     try:
         blog_post_id = int(blog_post_id)
     except ValueError:
-        print("Invalid blog post ID.")
+        print(ERROR_COLOR + "Invalid blog post ID.")
         return
 
     if blog_post_id == 0:
@@ -143,9 +154,9 @@ def delete_blog_post(user):
     if blog_post:
         session.delete(blog_post)
         session.commit()
-        print("Blog post deleted successfully.")
+        print(SUCCESS_COLOR + "Blog post deleted successfully.")
     else:
-        print("Invalid blog post ID or you do not have permission to delete this post.")
+        print(ERROR_COLOR + "Invalid blog post ID or you do not have permission to delete this post.")
 
 def main():
     user = None
@@ -156,7 +167,7 @@ def main():
         print("2. Sign In")
         print("0. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = input(PROMPT_COLOR + "Enter your choice: ")
 
         if choice == "1":
             sign_up()
@@ -165,7 +176,7 @@ def main():
         elif choice == "0":
             exit()
         else:
-            print("Invalid choice. Please enter a number from the menu.")
+            print(ERROR_COLOR + "Invalid choice. Please enter a number from the menu.")
 
     while True:
         print("\nMenu:")
@@ -175,7 +186,7 @@ def main():
         print("6. Delete Blog Post")
         print("0. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = input(PROMPT_COLOR + "Enter your choice: ")
 
         if choice == "3":
             view_all_blog_posts()
@@ -191,11 +202,11 @@ def main():
                     table.add_row([post.id, post.title, post.created_at])
                 print(table)
 
-                blog_post_id = input("Enter blog post ID to view details (or 0 to go back): ")
+                blog_post_id = input(PROMPT_COLOR + "Enter blog post ID to view details (or 0 to go back): ")
                 try:
                     blog_post_id = int(blog_post_id)
                 except ValueError:
-                    print("Invalid blog post ID.")
+                    print(ERROR_COLOR + "Invalid blog post ID.")
                     continue
 
                 if blog_post_id != 0:
@@ -203,7 +214,7 @@ def main():
                     if blog_post:
                         view_blog_post_details(blog_post)
                     else:
-                        print("Invalid blog post ID or you do not have permission to view this post.")
+                        print(ERROR_COLOR + "Invalid blog post ID or you do not have permission to view this post.")
         elif choice == "5":
             add_blog_post(user)
         elif choice == "6":
@@ -211,7 +222,7 @@ def main():
         elif choice == "0":
             break
         else:
-            print("Invalid choice. Please enter a number from the menu.")
+            print(ERROR_COLOR + "Invalid choice. Please enter a number from the menu.")
 
 if __name__ == "__main__":
     main()
